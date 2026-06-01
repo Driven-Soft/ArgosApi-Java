4d2fa8459a3814bd52b12c684ecce0da123d4b37
Henrique <henriquect08@gmail.com>
2026-06-01 19:03:00 -0300

    feat: estruturação inicial do projeto Spring Boot

---
+++ src/main/java/br/com/fiap/java/ArgosApi/ArgosApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.java.ArgosApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArgosApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArgosApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fiap/java/ArgosApi/ArgosApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.java.ArgosApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArgosApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
